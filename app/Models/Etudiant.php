64226c3cb349012665681319183c753c801dbcee
ELBHIHE <116025067+ELBHIHE@users.noreply.github.com>
2023-02-15 13:53:28 +0100

    ajouter les relations

--- app/Models/Etudiant.php
+++ app/Models/Etudiant.php
@@ -8,5 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Etudiant extends Model
 {
     use HasFactory;
-    protected $fillable = ['nom', 'filiere_id'];
+    
+   protected $fillable = ['nom', 'prenom', 'email', 'password', 'filiere_id'];
+
+    public function filiere()
+    {
+        return $this->belongsTo(Filiere::class);
+    }
+
+    public function examens()
+    {
+        return $this->belongsToMany(Examen::class)->withPivot('valeur');
+    }
+
+    public function notes()
+    {
+        return $this->hasMany(Note::class);
+    }
 }
