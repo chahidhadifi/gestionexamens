5f101a1f318fa0093bc357b0773ba04f090769dc
ELBHIHE <116025067+ELBHIHE@users.noreply.github.com>
2023-02-15 13:56:33 +0100

    ajouter les relations

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,5 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
-    protected $fillable = ['examen_id', 'etudiant_id', 'valeur'];
+    protected $fillable = [
+        'note', 'etudiant_id', 'examen_id'
+    ];
+
+    public function examen()
+    {
+        return $this->belongsTo(Examen::class);
+    }
+
+    public function etudiant()
+    {
+        return $this->belongsTo(Etudiant::class);
+    }
+
 }
