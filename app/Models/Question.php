34f7863fe2f9af4cc34bb9c4cb5e74725d8d4278
ELBHIHE <116025067+ELBHIHE@users.noreply.github.com>
2023-02-15 13:58:43 +0100

    ajouter les relations

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,5 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
-    protected $fillable = ['libelle', 'examen_id'];
+     protected $fillable = [
+        'libelle','examen_id'
+    ];
+
+    public function examen()
+    {
+        return $this->belongsTo(Examen::class);
+    }
+
+    public function propositions()
+    {
+        return $this->hasMany(Proposition::class);
+    }
+
+    public function reponses()
+    {
+        return $this->hasMany(Reponse::class);
+    }
 }
