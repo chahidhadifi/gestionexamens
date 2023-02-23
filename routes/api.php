9e69527c2b462138abe4d2b2bd25d79d9e66ea1d
Chahid Hadifi <chahidhadifi@gmail.com>
2023-02-23 23:21:35 +0100

    protection des routes

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,7 @@ use App\Http\Controllers\ReponseController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+// Public routes
 Route::apiResource('filieres',FiliereController::class);
 Route::apiResource('etudiants',EtudiantController::class);
 Route::apiResource('departements',DepartementController::class);
@@ -40,3 +37,47 @@ Route::apiResource('propositions',PropositionController::class);
 Route::apiResource('questions',QuestionController::class);
 Route::apiResource('reponses',ReponseController::class);
 Route::apiResource('notes',NoteController::class);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Departement
+    Route::post('/departements',[DepartementController::class, 'store']);
+    Route::put('/departements/{id}',[DepartementController::class, 'update']);
+    Route::delete('/departements/{id}',[DepartementController::class, 'destroy']);
+    // Etudiant
+    Route::post('/etudiants',[EtudiantController::class, 'store']);
+    Route::put('/etudiants/{id}',[EtudiantController::class, 'update']);
+    Route::delete('/etudiants/{id}',[EtudiantController::class, 'destroy']);
+    // Examen
+    Route::post('/examens',[ExamenController::class, 'store']);
+    Route::put('/examens/{id}',[ExamenController::class, 'update']);
+    Route::delete('/examens/{id}',[ExamenController::class, 'destroy']);
+    // Filiere
+    Route::post('/filieres',[FiliereController::class, 'store']);
+    Route::put('/filieres/{id}',[FiliereController::class, 'update']);
+    Route::delete('/filieres/{id}',[FiliereController::class, 'destroy']);
+    // Matiere
+    Route::post('/matieres',[MatiereController::class, 'store']);
+    Route::put('/matieres/{id}',[MatiereController::class, 'update']);
+    Route::delete('/matieres/{id}',[MatiereController::class, 'destroy']);
+    // Note
+    Route::post('/notes',[NoteController::class, 'store']);
+    Route::put('/notes/{id}',[NoteController::class, 'update']);
+    Route::delete('/notes/{id}',[NoteController::class, 'destroy']);
+    // Professeur
+    Route::post('/professeurs',[ProfesseurController::class, 'store']);
+    Route::put('/professeurs/{id}',[ProfesseurController::class, 'update']);
+    Route::delete('/professeurs/{id}',[ProfesseurController::class, 'destroy']);
+    // Proposition
+    Route::post('/propositions',[PropositionController::class, 'store']);
+    Route::put('/propositions/{id}',[PropositionController::class, 'update']);
+    Route::delete('/propositions/{id}',[PropositionController::class, 'destroy']);
+    // Question
+    Route::post('/questions',[QuestionController::class, 'store']);
+    Route::put('/questions/{id}',[QuestionController::class, 'update']);
+    Route::delete('/questions/{id}',[QuestionController::class, 'destroy']);
+    // Reponse
+    Route::post('/reponses',[ReponseController::class, 'store']);
+    Route::put('/reponses/{id}',[ReponseController::class, 'update']);
+    Route::delete('/reponses/{id}',[ReponseController::class, 'destroy']);
+});
